09ad61d6c390aaa612c965eb0cd493f71ab55fcb
Cristóbal Gajardo Vera <cristobalgajardo.acidlabs@latam.com>
2025-07-17 17:18:10 -0400

    build: initial commit

---
+++ src/main/java/cl/latam/contactabilitytest/ContactabilityTestApplication.java
@@ -0,0 +1,13 @@
+package cl.latam.contactabilitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactabilityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactabilityTestApplication.class, args);
+	}
+
+}
